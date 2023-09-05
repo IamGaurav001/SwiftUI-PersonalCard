65783cb358e72a1a7afbd3af9e57bcd02b5cfafd
IamGaurav001 <137531662+IamGaurav001@users.noreply.github.com>
2023-09-05 14:26:53 +0530

    Initial Commit

---
+++ Gaurav card/Gaurav_cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gaurav_cardApp.swift
+//  Gaurav card
+//
+//  Created by Gaurav Kumar Dubey on 05/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Gaurav_cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
